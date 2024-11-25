7968e60daf62123b3b7374f3ac22d591d3b0b0b6
GuglioIsStupid <bradsbluetab@gmail.com>
2024-11-25 15:30:21 -0500

    add this back

--- src/Engine/PrebuiltDLL/init.lua
+++ src/Engine/PrebuiltDLL/init.lua
@@ -1,4 +1,4 @@
---[[ local path = ... .. "."
+local path = ... .. "."
 -- replace all . with / 
 path = string.gsub(path, "%.", "/")
 local os = love.system.getOS()
@@ -13,8 +13,8 @@ if os == "Windows" then
         -- copy all files in ./Win64 to DLL/
         local files = love.filesystem.getDirectoryItems(path .. "Win64")
         for _, file in ipairs(files) do
-            if not love.filesystem.getInfo(file) then
-                love.filesystem.write(file, love.filesystem.read(path .. "Win64/" .. file))
+            if not love.filesystem.getInfo("DLL/" .. file) then
+                love.filesystem.write("DLL/" .. file, love.filesystem.read(path .. "Win64/" .. file))
             end
         end
     else
@@ -25,8 +25,8 @@ elseif os == "OS X" then
         -- copy all files in ./Mac64 to DLL/
         local files = love.filesystem.getDirectoryItems(path .. "Mac64")
         for _, file in ipairs(files) do
-            if not love.filesystem.getInfo(file) then
-                love.filesystem.write(file, love.filesystem.read(path .. "Mac64/" .. file))
+            if not love.filesystem.getInfo("DLL/" .. file) then
+                love.filesystem.write("DLL/" .. file, love.filesystem.read(path .. "Mac64/" .. file))
             end
         end
     else
@@ -37,8 +37,8 @@ elseif os == "Linux" then
         -- copy all files in ./Linux64 to DLL/
         local files = love.filesystem.getDirectoryItems(path .. "Linux64")
         for _, file in ipairs(files) do
-            if not love.filesystem.getInfo(file) then
-                love.filesystem.write(file, love.filesystem.read(path .. "Linux64/" .. file))
+            if not love.filesystem.getInfo("DLL/" .. file) then
+                love.filesystem.write("DLL/" .. file, love.filesystem.read(path .. "Linux64/" .. file))
             end
         end
     else
@@ -46,12 +46,25 @@ elseif os == "Linux" then
     end
 else
     debug.warn("UNSUPPORTED OS! " .. os)
-end ]]
+end
+
+local savepath = love.filesystem.getSaveDirectory()
+if os == "Windows" then
+    package.cpath = package.cpath .. ";" .. savepath .. "/DLL/?.dll"
+elseif os == "OS X" then
+    package.cpath = package.cpath .. ";" .. savepath .. "/DLL/?.dylib"
+elseif os == "Linux" then
+    package.cpath = package.cpath .. ";" .. savepath .. "/DLL/?.so"
+end
+--package.cpath = package.cpath .. ";" .. savepath .. "/DLL/?.dll"
 
 tryExcept(function()
     DLL_Video = require("video")
 end)
 
 tryExcept(function()
+    if os ~= "Windows" then
+        return
+    end
     DLL_Https = require("https")
 end)
