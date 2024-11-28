7ca0004e265b1686b813ef065af7d42dcbabb957
GuglioIsStupid <bradsbluetab@gmail.com>
2024-11-28 10:40:43 -0500

    more playfield stuff (TODO: Modchart stuff?)

--- src/Game/Objects/Game/Mania/Playfield.lua
+++ src/Game/Objects/Game/Mania/Playfield.lua
@@ -3,8 +3,31 @@ local Playfield = Group:extend("Playfield")
 function Playfield:new(underlay, receptors)
     Group.new(self)
 
+    self.notes = {}
+
     self:add(underlay)
     self:add(receptors)
+end
+
+function Playfield:add(obj)
+    if obj:isInstanceOf(HitObject) then
+        table.insert(self.notes, obj)
+    end
+
+    Group.add(self, obj)
+end
+
+function Playfield:remove(obj)
+    if obj:isInstanceOf(HitObject) then
+        for i, note in ipairs(self.notes) do
+            if note == obj then
+                table.remove(self.notes, i)
+                break
+            end
+        end
+    end
+
+    Group.remove(self, obj)
 end
 
 return Playfield
