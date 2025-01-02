975e7644c3375c4888eeaf7a134d68e69d90c104
GuglioIsStupid <bradsbluetab@gmail.com>
2025-01-02 12:44:04 -0500

    Update scrollspeed to ms on screen

--- src/Game/Managers/General/SettingsManager.lua
+++ src/Game/Managers/General/SettingsManager.lua
@@ -26,20 +26,61 @@ local SettingsDefault = {
         Master = 1,
         Effects = 0.15,
         Music = 1,
+    },
+    Metadata = {
+        SettingsVersion = 1
     }
 }
+
+function SettingsManager:migrate__ver0__settings(settings)
+    -- Scrollspeed update
+    -- update old scrollspeed system, to ms on screen
+    local speed = settings.Game.ScrollSpeed
+    local pixelsPerMS = (speed/10) / 5
+    local msOnScreen = Game._windowHeight / pixelsPerMS
+
+    settings.Game.ScrollSpeed = msOnScreen
+    settings.Metadata.SettingsVersion = 1
+end
+
+function SettingsManager:checkVersion(ver)
+    if ver then
+        if ver < SettingsDefault.Metadata.SettingsVersion then
+            return false
+        end
+    else
+        return false
+    end
+
+    return true
+end
 
 function SettingsManager:loadSettings()
     local exists = love.filesystem.getInfo(tempSettingsFilePath)
     if exists then
         self._settings = Json.decode(love.filesystem.read(tempSettingsFilePath))
 
+        local version = self._settings.Metadata and self._settings.Metadata.SettingsVersion or 0
+
         for category, settings in pairs(SettingsDefault) do
             for setting, value in pairs(settings) do
+                if not self._settings[category] then
+                    self._settings[category] = {}
+                end
                 if self._settings[category][setting] == nil then
                     self._settings[category][setting] = value
                 end
             end
+        end
+
+        if not self:checkVersion(version) then
+            -- version not correct. Update version from which one
+            if version == 0 then
+                self:migrate__ver0__settings(self._settings)
+            end
+
+            FileHandler:writeEncryptedFile(settingsFilePath, Json.encode(self._settings))
+            love.filesystem.write(tempSettingsFilePath, Json.encode(self._settings))
         end
     else
         FileHandler:writeEncryptedFile(settingsFilePath, Json.encode(SettingsDefault))
@@ -59,11 +100,10 @@ end
 
 function SettingsManager:getSetting(category, setting)
     if category == "Game" and setting == "ScrollSpeed" then
+        -- convert ms on screen to pixelsPerMS
         local speed = self._settings[category][setting]
-        -- the base game height is 1080
-        -- the scrollspeed is made with 1080 in mind
-        -- convert it properly to the current window height
-        return (speed/10) / 5 * (1080/Game._windowHeight)
+        local pixelsPerMS = Game._windowHeight / speed
+        return pixelsPerMS
     else
         return self._settings[category][setting]
     end
