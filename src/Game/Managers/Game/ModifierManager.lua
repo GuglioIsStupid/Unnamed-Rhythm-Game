fdf0562f1e9e5e6bdd50e72d0be0d3d73b6cc85f
GuglioIsStupid <bradsbluetab@gmail.com>
2025-01-02 19:37:15 -0500

    ModifierManager bits and pieces

--- src/Game/Managers/Game/ModifierManager.lua
+++ src/Game/Managers/Game/ModifierManager.lua
@@ -1,22 +1,27 @@
 local ModifierManager = Class:extend("ModifierManager")
 
 ModifierManager.Modifiers = {
-    -- {Name, Description, ScoreMultAddition}
+    -- {Name, Description, ScoreMultAddition, Shorthand}
     {
         "No LN", "Disables Long Notes", -0.25, -- applies if the map has long notes
+        "NLN"
     },
     {
         "No SV", "Disables Scroll Velocity", -0.25, -- applies if the map has a scroll velocity
+        "NSV"
     },
     {
         "Fade Out", "Notes fade out as they approach the receptors", 0.1,
+        "FO"
     },
     {
         "Fade In", "Notes fade in as they approach the receptors", 0.1,
+        "FI"
     },
     -- if both fade in and fade out are enabled, then multiplier goes up .3
     {
         "Mirror", "Notes are mirrored (Left -> Right, Up -> Down)", 0,
+        "M"
     }
 }
 
@@ -27,6 +32,22 @@ function ModifierManager:getModifier(name)
     for _, mod in ipairs(self.Modifiers) do
         if mod[1] == name then
             return mod
+        end
+    end
+end
+
+function ModifierManager:getShortHandFromName(name)
+    for _, mod in ipairs(self.Modifiers) do
+        if mod[1] == name then
+            return mod[4]
+        end
+    end
+end
+
+function ModifierManager:getNameFromShorthand(shorthand)
+    for _, mod in ipairs(self.Modifiers) do
+        if mod[4] == shorthand then
+            return mod[1]
         end
     end
 end
@@ -51,7 +72,12 @@ function ModifierManager:getScoreMultiplier()
     end
 
     for _, mod in ipairs(self.ActiveModifiers) do
+        if mod == "Fade Out" or mod == "Fade In" then
+            goto continue
+        end
         mult = mult + self:getModifier(mod)[3]
+
+        ::continue::
     end
 
     return mult
